6f7a7b81cae03fa94dd686fc6573b0e13b3533ba
moom-spotter <duddnd2574@spotter.kr>
2024-11-13 14:43:01 +0900

    Add 'RankingList' component'

--- poke-n-pump/app/(tabs)/ranking.tsx
+++ poke-n-pump/app/(tabs)/ranking.tsx
@@ -1,11 +1,12 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { StyleSheet } from 'react-native';
+import RankingList from '@/components/tabs/ranking/RankingList';
 
 export default function RankingScreen() {
   return (
     <ThemedView style={styles.rankingView}>
-      <ThemedText>Ranking Screen</ThemedText>
+      <RankingList />
     </ThemedView>
   );
 }
---
+++ poke-n-pump/components/tabs/ranking/RankingList.tsx
@@ -0,0 +1,63 @@
+import { StyleSheet, Image } from 'react-native';
+import { ThemedView } from '../../ThemedView';
+import { ThemedText } from '../../ThemedText';
+import { ThemedScrollView } from '../../ThemedScrollView';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
+
+export default function RankingList() {
+    const colorScheme = useColorScheme();
+
+    const themeColor = Colors[colorScheme ?? 'light'];
+
+    const rankings = [
+        { name: 'Paul', xp: 1500 },
+        { name: 'David', xp: 1500 },
+        { name: 'Nina', xp: 1500 },
+        { name: 'Peter', xp: 1500 },
+        { name: 'Lynn', xp: 1500 },
+        { name: 'Chan', xp: 1500 },
+        { name: 'Sejun', xp: 1500 },
+        { name: 'Clara', xp: 1500 },
+        { name: 'Mewtwo', xp: 1500 },
+        { name: 'Mew', xp: 1500 },
+    ];
+
+    return (
+        <ThemedView style={styles.rankingListView}>
+            <ThemedScrollView style={styles.rankingsContainer} showsVerticalScrollIndicator={false}>
+                { rankings.map((ranking, index) => (
+                    <ThemedView
+                        key={index}
+                        style={styles.rankingContainer}
+                        lightColor={themeColor.mainLight}
+                        darkColor={themeColor.mainLight}
+                        lightBorderColor={themeColor.mainLight}
+                        darkBorderColor={themeColor.mainLight}
+                    >
+                        <ThemedText type='default' lightColor={themeColor.reverse} darkColor={themeColor.reverse}>{ranking.name}</ThemedText>
+                        <ThemedText type='default' lightColor={themeColor.reverse} darkColor={themeColor.reverse}>{ranking.xp}XP</ThemedText>
+                    </ThemedView>
+                )) }
+            </ThemedScrollView>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    rankingListView: {
+        height: '70%',
+        width: '90%',
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    rankingsContainer: {
+        width: '100%',
+    },
+    rankingContainer: {
+        margin: 5,
+        padding: 10,
+        borderWidth: 5,
+        borderRadius: 10,
+    },
+});
