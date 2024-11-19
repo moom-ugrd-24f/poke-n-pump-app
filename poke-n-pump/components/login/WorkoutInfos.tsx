fb9cca44133431d241c05a5a2449f0cd979d4122
moom-spotter <duddnd2574@spotter.kr>
2024-11-19 16:18:38 +0900

    Reuse 'WorkoutSchedule' component in login page

--- poke-n-pump/components/login/WorkoutInfos.tsx
+++ poke-n-pump/components/login/WorkoutInfos.tsx
@@ -5,44 +5,17 @@ import { Ionicons } from '@expo/vector-icons';
 import { ThemedText } from '../ThemedText';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import WorkoutSchedule from '../WorkoutSchedule';
 
 export default function ProfileInfos() {
   const colorScheme = useColorScheme();
 
   const themeColor = Colors[colorScheme ?? 'light'];
 
-  const [workoutSchedule, setWorkoutSchedule] = useState([
-    { day: 'S', workoutSession: false, color: 'red' },
-    { day: 'M', workoutSession: false },
-    { day: 'T', workoutSession: false },
-    { day: 'W', workoutSession: false },
-    { day: 'T', workoutSession: false },
-    { day: 'F', workoutSession: false },
-    { day: 'S', workoutSession: false, color: 'blue' },
-  ]);
-
-  const toggleWorkoutSession = (index: number) => {
-    setWorkoutSchedule((prevSchedule) =>
-      prevSchedule.map((session, i) =>
-        i === index ? { ...session, workoutSession: !session.workoutSession } : session
-      )
-    );
-  };
-
   return (
     <ThemedView style={styles.visibilityView}>
         <ThemedText type='title'>Choose your visibility setting</ThemedText>
-        <ThemedView style={styles.workoutSchedule} lightBorderColor={themeColor.mainDark} darkBorderColor={themeColor.mainDark}>
-            {workoutSchedule.map((session, index) => (
-                <ThemedView key={index}>
-                    <ThemedText type='title' lightColor={session.color}>{session.day}</ThemedText>
-                    <Ionicons 
-                    name={session.workoutSession ? 'checkbox-outline' : 'stop-outline'} 
-                    size={50} color={themeColor.sub} 
-                    onPress={() => toggleWorkoutSession(index) }/>
-                </ThemedView>
-            ))}
-        </ThemedView>
+        <WorkoutSchedule />
     </ThemedView>
   );
 }
@@ -54,15 +27,5 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'space-between',
-    },
-    workoutSchedule: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderStyle: 'dashed',
-        borderWidth: 2,
-        borderRadius: 20,
-        padding: 10,
     }
 });
