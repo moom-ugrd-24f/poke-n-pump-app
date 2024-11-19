e26def4d4ee87a878264d0282dd0755128baad33
moom-spotter <duddnd2574@spotter.kr>
2024-11-19 16:57:25 +0900

    Replace setting tab components with storage-enabled components

--- poke-n-pump/components/tabs/settings/VisibilityOption.tsx
+++ poke-n-pump/components/tabs/settings/VisibilityOption.tsx
@@ -1,40 +1,14 @@
 import { StyleSheet } from 'react-native';
 import { ThemedView } from '../../ThemedView';
-import { useState } from 'react';
-import { Ionicons } from '@expo/vector-icons';
 import { ThemedText } from '../../ThemedText';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import VisibilityToggle from '@/components/VisibilityToggle';
 
 export default function VisibilityOption() {
-  const colorScheme = useColorScheme();
-  const [isPrivate, setIsPrivate] = useState<boolean>(false);
-
-  const themeColor = Colors[colorScheme ?? 'light'];
 
   return (
     <ThemedView style={styles.visibilityView}>
         <ThemedText type='subtitle'>Visibility setting</ThemedText>
-        <ThemedView 
-        style={styles.optionView} 
-        lightColor={themeColor.default} 
-        darkColor={themeColor.default}
-        lightBorderColor={isPrivate ? themeColor.main : themeColor.default}
-        darkBorderColor={isPrivate ? themeColor.main : themeColor.default}
-        onPress={() => setIsPrivate(true)}>
-            <Ionicons name="person" size={24} color={themeColor.reverse} />
-            <ThemedText lightColor={themeColor.reverse} darkColor={themeColor.reverse}>Friends</ThemedText>
-        </ThemedView>
-        <ThemedView 
-        style={styles.optionView}
-        lightColor={themeColor.default}
-        darkColor={themeColor.default}
-        lightBorderColor={!isPrivate ? themeColor.main : themeColor.default}
-        darkBorderColor={!isPrivate ? themeColor.main : themeColor.default}
-        onPress={() => setIsPrivate(false)}>
-            <Ionicons name="earth" size={24} color={themeColor.reverse} />
-            <ThemedText lightColor={themeColor.reverse} darkColor={themeColor.reverse}>Anyone</ThemedText>
-        </ThemedView>
+        <VisibilityToggle />
     </ThemedView>
   );
 }
