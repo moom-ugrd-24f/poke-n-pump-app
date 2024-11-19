3dd73fba659a542836382515a8e6b8e1f3349133
moom-spotter <duddnd2574@spotter.kr>
2024-11-20 08:59:43 +0900

    Store username on clicking 'check' button

--- poke-n-pump/components/login/ProfileInfos.tsx
+++ poke-n-pump/components/login/ProfileInfos.tsx
@@ -19,44 +19,67 @@ export default function ProfileInfos() {
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
         });
 
         console.log(result);
 
         if (!result.canceled) {
-        setImage(result.assets[0].uri);
+            setImage(result.assets[0].uri);
         }
     };
 
-  return (
-    <ThemedView style={styles.profileView}>
-        <ThemedText type="title">Welcome to{" "}
-            <ThemedText 
-            type="title" 
-            lightColor={themeColor.sub} 
-            darkColor={themeColor.sub}>
-                Poke
-            </ThemedText>
-            &
-            <ThemedText
-            type="title" 
-            lightColor={themeColor.sub} 
-            darkColor={themeColor.sub}>
-                Pump
-            </ThemedText>
-            !
-        {"\n"}Let me know about you.</ThemedText>
-        { image ? <Image source={{ uri: image }} style={styles.image} /> : <Ionicons name="person-circle" size={150} color="white" onPress={pickImage} /> }
-        <ThemedView style={styles.username}>
-            <ThemedText type='subtitle' lightColor={themeColor.default} darkColor={themeColor.default}>Nickname</ThemedText>
-            <ThemedTextInput />
-        </ThemedView> 
-    </ThemedView>
-  );
+    const saveUsername = async () => {
+        console.log("Username : " + username);
+
+        if (username === null || username === '') {
+            return;
+        }
+
+        try {
+            await AsyncStorage.setItem('username', username);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return (
+        <ThemedView style={styles.profileView}>
+            <ThemedText type="title">Welcome to{" "}
+                <ThemedText 
+                    type="title" 
+                    lightColor={themeColor.sub} 
+                    darkColor={themeColor.sub}>
+                    Poke
+                </ThemedText>
+                &
+                <ThemedText
+                    type="title" 
+                    lightColor={themeColor.sub} 
+                    darkColor={themeColor.sub}>
+                    Pump
+                </ThemedText>
+                !
+            {"\n"}Let me know about you.</ThemedText>
+            { image ? <Image source={{ uri: image }} style={styles.image} /> : <Ionicons name="person-circle" size={150} color="white" onPress={pickImage} /> }
+            <ThemedView style={styles.username}>
+                <ThemedText type='subtitle' lightColor={themeColor.default} darkColor={themeColor.default}>username</ThemedText>
+                <ThemedView style={styles.usernameInput}>
+                    <ThemedTextInput onChangeText={setUsername} />
+                    <ThemedButton 
+                        title="Check"
+                        style={styles.checkButton}
+                        onPress={() => {
+                            saveUsername();
+                        }} 
+                    />
+                </ThemedView>
+            </ThemedView> 
+        </ThemedView>
+    );
 }
 
 const styles = StyleSheet.create({
