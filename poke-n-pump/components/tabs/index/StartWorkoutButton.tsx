12d8b98bd9069de821deabdf23f0e6305b6a0837
moom-spotter <duddnd2574@spotter.kr>
2024-11-20 17:00:06 +0900

    Post workout complete request on clicking 'Start Workout' button

--- poke-n-pump/components/tabs/index/StartWorkoutButton.tsx
+++ poke-n-pump/components/tabs/index/StartWorkoutButton.tsx
@@ -1,16 +1,23 @@
 import ThemedButton from '@/components/ThemedButton';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Colors } from '@/constants/Colors';
+import { USER_URL } from '@/constants/url';
 
 export default function StartWorkoutButton() {
     const colorScheme = useColorScheme();
+
+    function postWorkoutComplete() {
+        const user_id = '672b3de2613125bee0cdee3d';
+        const workout_complete_url = USER_URL + '/' + user_id + '/workout-complete';
+        fetch(workout_complete_url, { method: 'POST' })
+    }
 
     return (
         <ThemedButton
             title="Start Workout"
             lightBorderColor={Colors[colorScheme ?? 'light'].sub}
             darkBorderColor={Colors[colorScheme ?? 'light'].sub}
-            onPress={() => console.log('Starting workout...')}
+            onPress={() => postWorkoutComplete()}
         />
     );
 }
--- poke-n-pump/constants/url.ts
+++ poke-n-pump/constants/url.ts
@@ -4,4 +4,3 @@ export const API_BASE_URL = BASE_URL + '/api';
 export const USER_URL = API_BASE_URL + '/users';
 export const FREIEND_REQUEST_URL = API_BASE_URL + '/friend-requests';
 export const POKE_URL = API_BASE_URL + '/pokes';
-export const WORKOUT_COMPLETE_URL = USER_URL + '/672b3de2613125bee0cdee3d/complete-workout'
