a48be276045c409ce22043a63c77224fc1aaaed8
moom-spotter <duddnd2574@spotter.kr>
2024-12-01 16:13:10 +0900

    Update user information when pressing 'Apply Changes' button on settins page

--- poke-n-pump/app/(tabs)/settings.tsx
+++ poke-n-pump/app/(tabs)/settings.tsx
@@ -11,24 +11,65 @@ import { router } from 'expo-router';
 import { ThemedView } from '@/components/themedComponents/ThemedView';
 import { deleteUser } from '@/hooks/useAPI';
 import { getUserId } from '@/hooks/useAsyncStorage';
-import { useState, useEffect } from 'react';
+import { getUserInfo, updateAndStoreUser } from '@/hooks/useAPI';
+import { useState, useEffect, useCallback } from 'react';
+import { useFocusEffect } from 'expo-router';
+import Toast from 'react-native-root-toast';
 
 export default function SettingsScreen() {
   const colorScheme = useColorScheme();
   const themeColor = Colors[colorScheme ?? 'light'];
   const [userId, setUserId] = useState('');
+  const [workoutDays, setWorkoutDays] = useState([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (userId === '') {
+        getUserId().then((res) => {
+          console.log('User id: ', res);
+          setUserId(res || '');
+          fetchWorkoutDays(res);
+        });
+      } else {
+        fetchWorkoutDays(userId);
+      }
+    }, [])
+  );
 
   useEffect(() => {
-    getUserId().then((res) => {
-      setUserId(res || '');
-    });
-  }, []);
+    console.log("WorkoutDays: ", workoutDays);
+  }, [workoutDays]);
+
+  const fetchWorkoutDays = async (userId: string) => {
+    const userInfo = await getUserInfo(userId);
+    setWorkoutDays(userInfo.data.workoutPlan.daysOfWeek);
+  }
 
   const deleteUserData = async () => {
     await AsyncStorage.clear();
     if (userId === '') return;
     deleteUser(userId);
   }
+
+  const createApplyChangesAlert = () =>
+    Alert.alert('Apply Changes', 'Are you sure to apply changes?', [
+      {
+        text: 'OK',
+        onPress: () => {
+          updateAndStoreUser({ workoutPlan: { daysOfWeek: workoutDays } }, userId);
+          Toast.show('Changes applied!', {
+            duration: Toast.durations.SHORT,
+            position: Toast.positions.CENTER,
+            hideOnPress: true,
+            shadow: true,
+            animation: true,
+          });
+        }
+      },
+      {
+        text: 'Cancel',
+      },
+    ]);
 
   const createDeleteAccountAlert = () => 
     Alert.alert('Delete Account', 'Your data will be deleted. Are you sure to proceed?', [
@@ -44,24 +85,34 @@ export default function SettingsScreen() {
       },
     ]);
 
-
   return (
     <ThemedView style={styles.settingsView} >
       <ThemedText type='header'>
         Settings
       </ThemedText>
       <VisibilityOption />
-      <WorkoutSchedule />
+      <WorkoutSchedule workoutDays={workoutDays} setWorkoutDays={setWorkoutDays} />
       <ShameOption />
       <ThemedButton 
-      title="Delete Account"
-      lightColor={themeColor.alert}
-      darkColor={themeColor.alert}
-      lightBorderColor={themeColor.alert}
-      darkBorderColor={themeColor.alert}
-      lightTextColor={themeColor.white}
-      darkTextColor={themeColor.white}
-      onPress={createDeleteAccountAlert} />
+        title="Apply Changes"
+        lightColor={themeColor.sub}
+        darkColor={themeColor.sub}
+        lightBorderColor={themeColor.subDark}
+        darkBorderColor={themeColor.subDark}
+        lightTextColor={themeColor.reverse}
+        darkTextColor={themeColor.reverse}
+        onPress={createApplyChangesAlert}
+      />
+      <ThemedButton 
+        title="Delete Account"
+        lightColor={themeColor.alert}
+        darkColor={themeColor.alert}
+        lightBorderColor={themeColor.alert}
+        darkBorderColor={themeColor.alert}
+        lightTextColor={themeColor.white}
+        darkTextColor={themeColor.white}
+        onPress={createDeleteAccountAlert}
+      />
     </ThemedView>
   );
 }
